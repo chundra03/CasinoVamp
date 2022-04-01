a7c15cecaced70cb5b514290ea648ddb97c8099f
chundra03 <97737644+chundra03@users.noreply.github.com>
2022-04-01 09:46:54 +0300

    Add files via upload

---
+++ Casino/javascripts/java-script.js
@@ -0,0 +1,128 @@
+
+$( document ).ready(function(){
+	  $( ".fortuna" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+	    $( ".fortuna5" ).animate({
+	      rotate: "2000deg",
+	    }, {
+	      duration: 20000, // продолжительность анимации
+	      easing: "linear", // скорость анимации
+        animate: "infinite",
+	      complete: function(){ // callback
+	        $(".status").text("Анимация завершена");
+	      },
+	      queue: false // не ставим в очередь
+	    });
+	  });
+	});
+
+$( document ).ready(function(){
+		 $( ".fortunes2" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+		   $( ".fortunes22" ).animate({
+		     rotate: "2000deg",
+		   }, {
+		     duration: 20000, // продолжительность анимации
+		     easing: "linear", // скорость анимации
+	       animate: "infinite",
+		     complete: function(){ // callback
+		       $(".status").text("Анимация завершена");
+		      },
+		     queue: false // не ставим в очередь
+		    });
+		  });
+		});
+
+		$( document ).ready(function(){
+			  $( ".cub1" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+			    $( ".block1" ).animate({
+			      rotate: "2000deg",
+			    }, {
+			      duration: 20000, // продолжительность анимации
+			      easing: "linear", // скорость анимации
+		        animate: "infinite",
+			      complete: function(){ // callback
+			        $(".status").text("Анимация завершена");
+			      },
+			      queue: false // не ставим в очередь
+			    });
+			  });
+			});
+
+			$( document ).ready(function(){
+				  $( ".blocks3" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+				    $( ".block3" ).animate({
+				      rotate: "2000deg",
+				    }, {
+				      duration: 20000, // продолжительность анимации
+				      easing: "linear", // скорость анимации
+			        animate: "infinite",
+				      complete: function(){ // callback
+				        $(".status").text("Анимация завершена");
+				      },
+				      queue: false // не ставим в очередь
+				    });
+				  });
+				});
+
+				$( document ).ready(function(){
+					  $( ".blocks6" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+					    $( ".block6" ).animate({
+					      rotate: "2000deg",
+					    }, {
+					      duration: 20000, // продолжительность анимации
+					      easing: "linear", // скорость анимации
+				        animate: "infinite",
+					      complete: function(){ // callback
+					        $(".status").text("Анимация завершена");
+					      },
+					      queue: false // не ставим в очередь
+					    });
+					  });
+					});
+
+					$( document ).ready(function(){
+						  $( ".blocks2" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+						    $( ".block2" ).animate({
+						      rotate: "2000deg",
+						    }, {
+						      duration: 20000, // продолжительность анимации
+						      easing: "linear", // скорость анимации
+					        animate: "infinite",
+						      complete: function(){ // callback
+						        $(".status").text("Анимация завершена");
+						      },
+						      queue: false // не ставим в очередь
+						    });
+						  });
+						});
+
+						$( document ).ready(function(){
+								$( ".blocks5" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+									$( ".block5" ).animate({
+										rotate: "2000deg",
+									}, {
+										duration: 20000, // продолжительность анимации
+										easing: "linear", // скорость анимации
+										animate: "infinite",
+										complete: function(){ // callback
+											$(".status").text("Анимация завершена");
+										},
+										queue: false // не ставим в очередь
+									});
+								});
+							});
+
+							$( document ).ready(function(){
+									$( ".blocks" ).mouseover(function(){ // задаем функцию при нажатиии на элемент с классом start
+										$( ".block" ).animate({
+											rotate: "2000deg",
+										}, {
+											duration: 20000, // продолжительность анимации
+											easing: "linear", // скорость анимации
+											animate: "infinite",
+											complete: function(){ // callback
+												$(".status").text("Анимация завершена");
+											},
+											queue: false // не ставим в очередь
+										});
+									});
+								});
